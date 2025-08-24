58565716aeab5bf4135b045fda97a6d1150ac54c
Falco <tonyryabov99@gmail.com>
2025-08-24 08:56:00 +0200

    First commit

---
+++ calc.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def addNum(self):
+        return float(self.a+ self.b)
+    
+    def multiplyNum(self):
+        return float(self.a*self.b)
+    
+
+
+a = float(input("Enter the first number... "))
+b = float(input("Enter the second number... "))
+calc = Calculator(a,b)
+print(calc.addNum())
+print(calc.multiplyNum())
